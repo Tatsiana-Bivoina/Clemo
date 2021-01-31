b3d789a40efc6c7df7dc477214cb42565581797f
Tanya <tanya.urainbow@mail.ru>
2021-01-31 14:27:57 +0300

    add second slider, sprite.svg, gulp task for svg

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.swiper-container', {
+var swiperWrIntro = new Swiper('.intro-slider', {
     direction: 'vertical',
     speed: 1000,
     pagination: {
@@ -12,6 +12,27 @@ var swiper = new Swiper('.swiper-container', {
     autoplay: {
       delay: 3000,
       stopOnLastSlide: true,
+    },
+    grabCursor: true,
+  });
+  swiperWrIntro.on('click', function(){
+    alert('hello');
+  });
+
+  var swiperWrBestTeam = new Swiper('.best-team-slider', {
+    slideClass:'team-members',
+    slidesPerView: 1,
+    speed: 1000,
+    lazyLoading: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
     grabCursor: true,
   });
